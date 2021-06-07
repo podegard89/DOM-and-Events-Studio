b63ecb2211ce7d28b9868c5b67792b604b1f2382
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2021-06-07 15:40:30 -0500

    Take Off and Landing completed

--- scripts.js
+++ scripts.js
@@ -1,2 +1,29 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function() {
+    // buttons
+    let takeOffButton = document.getElementById("takeoff");
+    let landingButton = document.getElementById("landing");
+
+    // html elements we are affecting with our buttons
+    let flightStatus = document.getElementById("flightStatus");
+    let shuttleBackground = document.getElementById("shuttleBackground");
+    let spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+    
+    takeOffButton.addEventListener("click", function() {
+        let takeOffConfirmation = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        if(takeOffConfirmation === true) { // same as if(takeOffConfirmation)
+            flightStatus.innerHTML = "Shuttle in flight";  
+            shuttleBackground.style = "background-color: blue";
+            spaceShuttleHeight.innerHTML = 10000;
+        }
+    })
+
+    landingButton.addEventListener("click", function() {
+        alert("The shuttle is landing. Landing gear engaged.");
+        flightStatus.innerHTML = "The shuttle has landed.";
+        shuttleBackground.style = "background-color: green";
+        spaceShuttleHeight.innerHTML = 0;
+    })
+
+})
