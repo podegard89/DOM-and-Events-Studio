399af2bccbb04a21afadf3343a3d12a808708395
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2021-06-07 17:01:13 -0500

    Completed part 5 based on book solution

--- scripts.js
+++ scripts.js
@@ -5,6 +5,15 @@ window.addEventListener("load", function() {
     let takeOffButton = document.getElementById("takeoff");
     let landingButton = document.getElementById("landing");
     let abortButton = document.getElementById("missionAbort");
+    let upButton = document.getElementById("up");
+    let downButton = document.getElementById("down");
+    let rightButton = document.getElementById("right");
+    let leftButton = document.getElementById("left");
+    let rocketImage = document.getElementById("rocket");
+    rocketImage.style.position= 'absolute'; 
+    rocketImage.style.left = '0px'; 
+    rocketImage.style.bottom = '0px';
+
 
     // html elements we are affecting with our buttons
     let flightStatus = document.getElementById("flightStatus");
@@ -25,6 +34,9 @@ window.addEventListener("load", function() {
         flightStatus.innerHTML = "The shuttle has landed.";
         shuttleBackground.style = "background-color: green";
         spaceShuttleHeight.innerHTML = 0;
+        
+        // for part 5
+        rocketImage.style.bottom = "0px";
     })
 
     abortButton.addEventListener("click", function() {
@@ -33,7 +45,38 @@ window.addEventListener("load", function() {
             flightStatus.innerHTML = "Mission aborted.";
             shuttleBackground.style = "background-color: green";
             spaceShuttleHeight.innerHTML = 0;
+            // for part 5
+            rocketImage.style.bottom = "0px";
         }
+
+        
+    })
+
+    // Part 5 here. You don't have to know how this works.
+
+    upButton.addEventListener("click", function() {
+        let shuttleHeightNumber = Number(spaceShuttleHeight.innerHTML);
+        spaceShuttleHeight.innerHTML = shuttleHeightNumber + 10000;
+        // parseInt("0px") returns the number 0, parses string for an integer
+        let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
+        rocketImage.style.bottom = movement;
+    })
+
+    downButton.addEventListener("click", function() {
+        let shuttleHeightNumber = Number(spaceShuttleHeight.innerHTML);
+        spaceShuttleHeight.innerHTML = shuttleHeightNumber - 10000;
+        let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
+        rocketImage.style.bottom = movement;
+    })
+
+    rightButton.addEventListener("click", function() {
+        let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+        rocketImage.style.left = movement;
+    })
+
+    leftButton.addEventListener("click", function() {
+        let movement = parseInt(rocketImage.style.left) - 10 + 'px';
+        rocketImage.style.left = movement;
     })
 
 })
